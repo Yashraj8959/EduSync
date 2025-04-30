0b9926ed47e3fcc67b24c4036d97b114fdd830b6
Yashraj8959 <ykushwah7828@gmail.com>
2025-04-30 11:16:29 +0530

    Update loading message to indicate generation of industry insights

--- app/(main)/insights/layout.js
+++ app/(main)/insights/layout.js
@@ -10,7 +10,7 @@ const Layout = ({children}) => {
                     fallback={
                     // <BarLoader className='mt-4' width={"100%"} color='gray'/>
                     <div className="text-center text-gray-500 align-middle min-h-screen flex flex-col justify-center items-center">
-                    <p className="text-lg">Building Your Customized Dashboard...</p>
+                    <p className="text-lg">Generating Industry Insights...</p>
                     <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-indigo-500 mx-auto mt-4"></div> {/* Simple spinner */}
                   </div>
                   }
